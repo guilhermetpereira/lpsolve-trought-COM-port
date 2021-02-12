0a46d8fa3ab219012e8760e9eeec0bf464f7673e
Guilherme Tomaz Pereira <guilherme.tomazp@gmail.com>
2021-02-11 21:10:33 -0300

    implemented some syncronization methods

--- read_com.py
+++ read_com.py
@@ -1,6 +1,7 @@
 import serial
 import time 
 import lpsolve55
+import os
 from dataclasses import dataclass
 
 
@@ -21,37 +22,48 @@ class Node:
 
 
 def wait_command():
-	
+	ser.reset_input_buffer()
+	ser.reset_output_buffer()
 	s = ser.read(1)
-	
-	if s == b'S':
-		return True
-	else:
+	counter = 0
+	while s == b'S':
+		counter = counter +1
+		s = ser.read(1)
+	if counter!=4:
 		return False
+	else:
+		global total_nodes
+		total_nodes = int(''.join(s.decode('utf-8')),16)
+		return True
 
 def read_serial():
 	s = ser.read(1)
+	buffer.clear()
 	while s != b'T':
-		buffer.append(s)
+		if(s == b'S'):
+			buffer.clear()
+		else:
+			buffer.append(s)
 		s = ser.read(1)
-	ser.flushInput()
-	ser.flushOutput()
+	if(s == b'T'):
+		buffer.append(s)
+	ser.reset_input_buffer()
+	ser.reset_output_buffer()
 
 
 
 def process_data():
 	nodes_list.clear()
-	total_nodes = int(buffer.pop(0))
-	while len(buffer) != 0  and buffer[0] != b'F':
+	while len(buffer) != 0  and buffer[0] != b'T':
 		if buffer[0] == b'N':
 			buffer.pop(0)
 			assigned_ts = int(''.join(buffer.pop(0).decode('utf-8')),16)
-			
+
 			energy_str = [x.decode('utf-8') for x in buffer[0:8]]
 			energy_int = int(''.join(energy_str),16)
 			del buffer[0:8]
 			bitmap = []
-			for byte in range(-(total_nodes // -8)): # essa locura no range é só um ceil
+			for byte in range(-(total_nodes // -8)): # essa locura no range é só um ceil	
 				value = int(''.join(buffer.pop(0).decode('utf-8')),16)
 				for bit in range(7):
 					if (bool((0x01 << bit) & value)): 
@@ -61,7 +73,7 @@ def process_data():
 
 
 
-if __name__ == "__main__":	
+if __name__ == "__main__":
 	ser = serial.Serial(COM_PORT, BAUD_RATE)
 
 	if ser.is_open == False:
@@ -69,8 +81,8 @@ if __name__ == "__main__":
 			print('Failed to open PORT')
 		exit
 	
-	ser.flushInput()
-	ser.flushOutput()
+	ser.reset_input_buffer()
+	ser.reset_output_buffer()
 	state = 'STATE_WAIT_COMMAND'
 
 	while(1):
@@ -80,16 +92,18 @@ if __name__ == "__main__":
 				start_time = time.time()
 				read_serial()
 				process_data()
-				print("--- %s seconds ---" % (time.time() - start_time))
 				if ENABLE_PRINT:
+					print("--- %s seconds ---" % (time.time() - start_time))
 					print(nodes_list)	
-					pass		
 				state = 'STATE_WAIT_COMMAND'
 				# state = 'STATE_RX_STORE'
 			else:
-				state = 'STATE_IDLE'
+				print(ser.read_all())
+				state = 'STATE_WAIT_COMMAND'
 
 		elif state == 'STATE_SOLVE_COOP':
+			pass
+		elif state == 'STATE_IDLE':
 			pass
 		else: #STATE_IDLE
 			pass
